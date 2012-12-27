e10ee7abec65be85827d3ea94c809ea35cc93b6d
pasberth <pasberth@gmail.com>
2012-12-27 19:34:05 +0900

    fmfm

---
+++ examples/hello-world.sh
@@ -0,0 +1,5 @@
+cat <<EOF | unip-apply 
+unip/sym unip/kernel/echo
+unip/sym hoge
+unip/sym fuga
+EOF
---
+++ setup.sh
@@ -0,0 +1,3 @@
+export PATH=$PWD/bin:$PATH
+export UNIPLIB=$PWD/unip:$UNIPLIB
+export PERL5LIB=$PWD/perl:$PERL5LIB
